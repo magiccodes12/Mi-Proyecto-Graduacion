d512c08518a017bfb9fbfc110e8b3f1af816b36a
magiccodes12 <164654443+magiccodes12@users.noreply.github.com>
2024-10-04 18:57:47 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/donar', methods=['POST'])
+def donar():
+    cantidad = request.form.get('cantidad') 
+
+    print(f'Donacion recibida: ${cantidad}')
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
